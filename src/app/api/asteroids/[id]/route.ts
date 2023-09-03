7c62f6f21d25feea91a5b0937bd4801d884b8308
Gregory-Vinokur <grisha-vinokur@mail.ru>
2023-09-03 21:42:53 +0300

    feat: add force-dynamic to api routes

--- src/app/api/asteroids/[id]/route.ts
+++ src/app/api/asteroids/[id]/route.ts
@@ -1,6 +1,8 @@
 import { IApproachData } from "@/interfaces/IApproachData";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest, context: { params: { id: string } }) {
   try {
 
--- src/app/api/asteroids/route.ts
+++ src/app/api/asteroids/route.ts
@@ -1,6 +1,8 @@
 import { IAsteroidResponseData } from "@/interfaces/IAsteroidResponseData";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
